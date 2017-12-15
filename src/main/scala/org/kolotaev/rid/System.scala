bb36d47c87eed96bd0b1ca0a7c36e1f600d510a0
kolotaev <kolotaev@tut.by>
2017-12-15 23:07:04 +0300

    Add correct machine ID generation - with fallback

--- src/main/scala/org/kolotaev/rid/System.scala
+++ src/main/scala/org/kolotaev/rid/System.scala
@@ -2,23 +2,21 @@ package org.kolotaev.rid
 
 import scala.util.Random
 import java.security.MessageDigest
-import java.net.InetAddress
+import java.net.{InetAddress, UnknownHostException}
 
 object System {
   def machineID: Array[Byte] = {
-    val id = Array[Byte](3)
-//    val hostname = management.ManagementFactory.getRuntimeMXBean.getName.split("@")(1)
-    val hostname = InetAddress.getLocalHost.getHostName
-
-//    val m = MessageDigest.getInstance("MD5")
-//    val b = hostname.getBytes("UTF-8")
-//    m.update(b, 0, b.length)
-//    new java.math.BigInteger(1, m.digest()).toString(16).reverse.padTo(32, "0").reverse.mkString
-
-//    MessageDigest.getInstance("MD5").digest(hostname.getBytes)
-//    m.digest().map("%02x".format(_)).mkString -- slower
-
-    MessageDigest.getInstance("MD5").digest(hostname.getBytes).slice(0, 3)
+    try {
+      val hostname = InetAddress.getLocalHost.getHostName
+      MessageDigest.getInstance("MD5").digest(hostname.getBytes).slice(0, 3)
+    } catch {
+      case UnknownHostException => {
+        // Fallback to random number if hostname is unavailable
+        val bytes = Array[Byte](3)
+        Random.nextBytes(bytes)
+        bytes
+      }
+    }
   }
 
   def randomInt: Int = {
