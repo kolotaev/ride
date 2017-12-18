f63a23854fde121818de9d0bf18519fe91e12ad0
kolotaev <kolotaev@tut.by>
2017-12-18 17:00:16 +0300

    Add some ID parts tests

--- src/test/scala/org/kolotaev/rid/IdSpec.scala
+++ src/test/scala/org/kolotaev/rid/IdSpec.scala
@@ -17,14 +17,26 @@ class IdSpec extends FlatSpec with Matchers {
     id.machine should have length 3
   }
 
-  "ID" should "support parts extraction: PID" in {
-    val id: Id = Id()
-    id.pid should equal (37)
+  "ID's machine part" should "be the same for all Ids generated on the same machine" in {
+    val ids: Array[Id] = Array[Id](Id(), Id(), Id())
+    ids(0).machine should equal (ids(1).machine)
+    ids(1).machine should equal (ids(2).machine)
   }
 
-  "ID" should "support parts extraction: current counter" in {
-    val id: Id = Id()
-    id.counter should equal (3)
+  "ID's pid part" should "be the same for all IDs generated within the same process" in {
+    val ids: Array[Id] = Array[Id](Id(), Id(), Id())
+    ids(0).pid should equal (ids(1).pid)
+    ids(1).pid should equal (ids(2).pid)
+
+    ids(0).pid shouldBe equal (0)
+  }
+
+  "ID's counter part" should "increase monotonically" in {
+    val ids: Array[Id] = Array[Id](Id(), Id(), Id(), Id(), Id())
+    ids(0).counter should be < ids(1).counter
+    ids(1).counter should be < ids(2).counter
+    ids(2).counter should be < ids(3).counter
+    ids(3).counter should be < ids(4).counter
   }
 
 //  it should "throw NoSuchElementException if an empty stack is popped" in {
